caa1daf44327407bf3f7a0b8fdc682b52065c9eb
Kermishkov1996 <gtytr.n@yandex.ru>
2024-10-05 13:59:44 +0400

    creat grid.js

---
+++ grid.js
@@ -0,0 +1,12 @@
+const GRID_SIZE = 4;
+const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
+
+export class Grid {
+	constructor(gridElement) {
+		this.cell = [];
+		for (let i = 0; i < CELLS_COUNT; i++) {
+			const element = array[index];
+			
+		}
+	}
+}
