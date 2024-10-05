ff259a57c6c6f48dc6436abb485567fada8a5215
Kermishkov1996 <gtytr.n@yandex.ru>
2024-10-05 14:18:31 +0400

    import files

--- grid.js
+++ grid.js
@@ -1,13 +1,15 @@
+import { Cell } from "./cell.js";
+
 const GRID_SIZE = 4;
 const CELLS_COUNT = GRID_SIZE * GRID_SIZE;
 
 export class Grid {
 	constructor(gridElement) {
-		this.cell = [];
+		this.cells = [];
 		for (let i = 0; i < CELLS_COUNT; i++) {
 			this.cells.push(
-				new Cell(gridElement, x, y)
-			)
+				new Cell(gridElement, i % GRID_SIZE, Math.floor(i / GRID_SIZE))
+			);
 			
 		}
 	}
